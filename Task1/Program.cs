38c159c41684f4b57f0dc36ebff91d5e2fa368ac
Anton <B_atonyo_S@bk.ru>
2023-06-18 20:03:03 +0300

    TAsk1 completd

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿/*При помощи рекурсии ввести на экран натуральные числа до N*/
+using System;
+using static System.Console;
+void NaturalNumber (int LastNumber)
+{
+    Write($"{LastNumber} ");
+    if (LastNumber>0) NaturalNumber(LastNumber-1);
+    WriteLine();
+}
+Clear();
+NaturalNumber(10);
