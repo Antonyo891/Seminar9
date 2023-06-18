c911b42765fa382f48cc532ce16549163c325d0a
Anton <B_atonyo_S@bk.ru>
2023-06-18 20:56:40 +0300

    Task 3 completed

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача. Вернуть сумму цифр числа. Рекурсия*/
+using System;
+using static System.Console;
+int Enter(string n)
+{
+    System.Console.WriteLine(n);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int SumNumbers (int Numbers)
+{
+    if (Numbers/10==0) return Numbers;
+    int Sum = Numbers%10+SumNumbers(Numbers/10);
+    return Sum;
+}
+Clear();
+int n = Enter("Введите число");
+WriteLine($"Сумма цифр чисел ={SumNumbers(n)}");
